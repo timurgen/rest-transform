76e0c36cf54bed62aa7566068746981ffcca8fbd
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2018-01-10 10:36:17 +0100

    adding possibility to filter out entities that are filtered in sesame

--- service/transform-service.py
+++ service/transform-service.py
@@ -3,13 +3,24 @@ import json
 import os
 import requests
 from jinja2 import Template
+import logging
 
 app = Flask(__name__)
+logger = None
+format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logger = logging.getLogger('rest-transform')
+
+# Log to stdout
+stdout_handler = logging.StreamHandler()
+stdout_handler.setFormatter(logging.Formatter(format_string))
+logger.addHandler(stdout_handler)
+logger.setLevel(logging.INFO)
 
 prop = os.environ.get("PROPERTY", "response")
 method = os.environ.get("METHOD", "get")
 url_template = Template(os.environ["URL"])
 headers = json.loads(os.environ.get("HEADERS", "{}"))
+skip_filtered = os.environ.get("SKIP_FILTERED", "false")
 
 
 @app.route('/transform', methods=['POST'])
@@ -31,9 +42,18 @@ def receiver():
 
     # get entities from request
     entities = request.get_json()
-
-    # create the response
-    return Response(generate(entities), mimetype='application/json')
+    logger.info("Received %s entities from Sesam", len(entities))
+    if skip_filtered == "true":
+        logger.info("Filter flag is set - filtering out entities")
+        filtered = []
+        for entity in entities:
+            if not entity['_filtered']:
+                filtered.append(entity)
+        # create the response
+        logger.info("Amount of entities after filter %s", len(filtered))
+        return Response(generate(filtered), mimetype='application/json')
+    else:
+        return Response(generate(entities), mimetype='application/json')
 
 
 if __name__ == '__main__':
